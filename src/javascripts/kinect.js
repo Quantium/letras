0299e89e7645028ee5f80b90d66f1e2469b19850
Quantium <andres@quantium.com.mx>
2015-07-19 02:24:27 -0500

    Changing KinecticJS for KonvaJS (It's an updated fork) and resolving the disassociation of letters bug

--- src/javascripts/kinect.js
+++ src/javascripts/kinect.js
@@ -1,15 +1,15 @@
 /* KINECTJS */
-var stage = new Kinetic.Stage({
+var stage = new Konva.Stage({
   container: 'game',
   width: 578,
   height: 200
 });
 
-var layer = new Kinetic.Layer();
+var layer = new Konva.Layer();
 var rectX = stage.getWidth() / 2 - 50;
 var rectY = stage.getHeight() / 2 - 25;
 
-var simpleText = new Kinetic.Text({
+var simpleText = new Konva.Text({
   name: 'A',
   x: 0,
   y: 0,
@@ -19,7 +19,7 @@ var simpleText = new Kinetic.Text({
   fill: 'black'
 });
 
-var l_A = new Kinetic.Text({
+var l_A = new Konva.Text({
   x: stage.getWidth() / 2,
   y: 15,
   text: 'A',
@@ -30,7 +30,8 @@ var l_A = new Kinetic.Text({
   shadowBlur: 2,
   shadowOffset: 10,
   shadowOpacity: 0,
-  draggable: true/*,
+  draggable: true,
+  id: "A"/*,
   dragBoundFunc: function(pos) {
     return {
       x: pos.x,
@@ -39,7 +40,7 @@ var l_A = new Kinetic.Text({
   }*/
 });
 
-var l_B = new Kinetic.Text({
+var l_B = new Konva.Text({
   x: stage.getWidth() / 2,
   y: 25,
   text: 'B',
@@ -50,7 +51,8 @@ var l_B = new Kinetic.Text({
   shadowBlur: 2,
   shadowOffset: 10,
   shadowOpacity: 0,
-  draggable: true/*,
+  draggable: true,
+  id: "B"/*,
   dragBoundFunc: function(pos) {
     return {
       x: pos.x,
@@ -76,26 +78,26 @@ socket.on('dragstart', function (data) {
   writeMessage("Hay que DESAPARECER " + data.letter);
   var l = getLetter(data.letter);
   l.hide();
+  //l.off('dragstart');
+  //l.off('dragend');
+
   layer.draw();
 });
 socket.on('dragend', function (data) {
   writeMessage("Hay que APARECER " + data.letter + " en (" + data.x + "," + data.y + ")");
   var l = getLetter(data.letter);
-
-  var anim = new Kinetic.Animation(function(frame) {
-    l.setX(data.x);
-    l.setY(data.y);
-  }, layer);
-
-  anim.start();
+  l.x(data.x);
+  l.y(data.y);
 
   l.show();
+  //l.on('dragstart', letterDragStart);
+  //l.on('dragend', letterDragEnd);
   layer.draw();
 });
 
 function getLetter($id){
   for(i = 0; i < letters.length; i++){
-    if(letters[i]._id == $id){
+    if(letters[i].id() == $id){
       return letters[i];
     }
   }
@@ -108,14 +110,14 @@ function writeMessage(message) {
 }
 function letterDragStart(e) {
   writeMessage('dragstart');
-  var l = e.targetNode;
-  console.log(l._id);
-  socket.emit('dragstart',{letter:l._id,user:username});
+  var l = e.target;
+  console.log("lDS :: ",l.id());
+  socket.emit('dragstart',{letter:l.id()});
 }
 function letterDragEnd(e) {
   writeMessage('dragend');
-  var l = e.targetNode;
-  console.log(l.attrs.x);
-  socket.emit('dragend',{letter:l._id,user:username,x:l.attrs.x,y:l.attrs.y});
+  var l = e.target;
+  //console.log(l.attrs.x);
+  socket.emit('dragend',{letter:l.id(),x:l.x(),y:l.y()});
   //console.log(stage.toJSON());
 }
